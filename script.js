a349bc2933e139f672489f8a18e850c8896f20d5
Ev.s.o <110043767+Evgenij-evso@users.noreply.github.com>
2023-12-19 22:23:16 +0300

    first-commit

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let container_info_gl = document.querySelector('.container_info_gl')
+let container_trey = document.querySelector('.container_trey')
 let name_city = document.getElementById('name_city')
 let info_city = document.getElementById('info_city')
 let close_display_info = document.getElementById('close_display_info')
+let close_display_search = document.getElementById('close_display_search')
 
 let Chukotka = document.getElementById('Chukotka')
 let Yakutia = document.getElementById('Yakutia')
-let Krosnoyarsk = document.getElementById('Krosnoyarsk')
+let Krosnoyarsk = document.querySelectorAll('.KROSNAYARSK')
 
 function SizeDisplay(listCity){
     container_info_gl.style.display = 'flex'
@@ -21,10 +23,19 @@ function getInfoCity(nameCity){
         }
     }
 }
+close_display_search.addEventListener('click',function(){
+    container_trey.style.display = 'none'
+})
+$('#open_display_search').click(function(){
+    container_trey.style.display = 'flex'
+})
 close_display_info.addEventListener('click',function(){
     container_info_gl.style.display = 'none'
 })
 
-Krosnoyarsk.addEventListener('click',function(){
-    getInfoCity('Krosnoyarsk')
-})
+// Krosnayarsk_______________________________________________
+for(let i = 0; i < Krosnoyarsk.length; i++){
+    $(Krosnoyarsk[i]).click(function(){
+        getInfoCity('Krosnoyarsk')
+    })
+}
